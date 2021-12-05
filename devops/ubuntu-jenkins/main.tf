a777193daa303d97bb5ef113f04b847dcab068aa
ithanos460 <thanosjohn7@hotmail.com>
2021-12-05 13:50:32 +0200

    update jenkins vm

--- devops/ubuntu-jenkins/main.tf
+++ devops/ubuntu-jenkins/main.tf
@@ -69,12 +69,12 @@ terraform {
     location              = azurerm_resource_group.main.location
     resource_group_name   = azurerm_resource_group.main.name
     network_interface_ids = [azurerm_network_interface.main.id]
-    vm_size               = "Standard_B1ls"  # Standard_DS1_v2"
+    vm_size               = "Standard_B2s"  # Standard_DS1_v2"
   
     storage_image_reference {
       publisher = "Canonical"
       offer     = "UbuntuServer"
-      sku       = "16.04-LTS"
+      sku       = "18.04-LTS"
       version   = "latest"
     }
   
