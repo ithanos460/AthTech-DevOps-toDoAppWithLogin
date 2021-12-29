d9cdd837eb55328d2cd08f892b2cf838f98f8db4
nikpolik <n.a.polikandriotis@gmail.com>
2021-12-29 13:36:38 +0200

    Remove hardcoded password from terraform

--- devops/ubuntu-jenkins/main.tf
+++ devops/ubuntu-jenkins/main.tf
@@ -94,8 +94,8 @@ resource "azurerm_virtual_machine" "main" {
 
   os_profile {
     computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
   }
 
   os_profile_linux_config {
