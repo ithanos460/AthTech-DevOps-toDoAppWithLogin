334c9c65fd922ccfabb8374288a18b4a221fe2ed
nikpolik <n.a.polikandriotis@gmail.com>
2021-12-29 13:35:07 +0200

    Remove hardcoded password from terraform

--- devops/ubuntu-jenkins/main.tf
+++ devops/ubuntu-jenkins/main.tf
@@ -1,121 +1,163 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
-    required_providers {
-      azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=2.46.0"
-      }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
-    cloud {
-      organization = "athtech-devops-todo-with-login"
+  }
+  cloud {
+    organization = "athtech-devops-todo-with-login"
 
-      workspaces {
-        name = "production"
-      }
+    workspaces {
+      name = "production"
     }
   }
-  
-  # Configure the Microsoft Azure Provider
-  provider "azurerm" {
-    features {}
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "main" {
+  name     = "devops-ng-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-virtualNetwork1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-pip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
-  
-  # Create a resource group
-  resource "azurerm_resource_group" "main" {
-    name     = "devops-ng-rg"
-    location = "West Europe"
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm-staging"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B2s" # Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
-  
-  resource "azurerm_virtual_network" "main" {
-    name                = "${var.prefix}-virtualNetwork1"
-    location            = azurerm_resource_group.main.location
-    resource_group_name = azurerm_resource_group.main.name
-    address_space       = ["10.0.0.0/16"]
+
+  storage_os_disk {
+    name              = "ngosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
-  
-  resource "azurerm_subnet" "main" {
-    name                 = "${var.prefix}-subnet"
-    resource_group_name  = azurerm_resource_group.main.name
-    virtual_network_name = azurerm_virtual_network.main.name
-    address_prefixes     = ["10.0.1.0/24"]
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
   }
-  
-  resource "azurerm_public_ip" "main" {
-    name                    = "${var.prefix}-pip"
-    location                = azurerm_resource_group.main.location
-    resource_group_name     = azurerm_resource_group.main.name
-    allocation_method       = "Static"
+
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
-  
-  resource "azurerm_network_security_group" "main" {
-    name                = "acceptanceTestSecurityGroup1"
-    location            = azurerm_resource_group.main.location
-    resource_group_name = azurerm_resource_group.main.name
-  
-    security_rule {
-      name                       = "SSH"
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "*"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm-production"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B2s" # Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
-  
-  resource "azurerm_subnet_network_security_group_association" "main" {
-    subnet_id                 = azurerm_subnet.main.id
-    network_security_group_id = azurerm_network_security_group.main.id
+
+  storage_os_disk {
+    name              = "ngosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
-  
-  resource "azurerm_virtual_machine" "main" {
-    name                  = "${var.prefix}-vm"
-    location              = azurerm_resource_group.main.location
-    resource_group_name   = azurerm_resource_group.main.name
-    network_interface_ids = [azurerm_network_interface.main.id]
-    vm_size               = "Standard_B2s"  # Standard_DS1_v2"
-  
-    storage_image_reference {
-      publisher = "Canonical"
-      offer     = "UbuntuServer"
-      sku       = "18.04-LTS"
-      version   = "latest"
-    }
-  
-    storage_os_disk {
-      name              = "ngosdisk1"
-      caching           = "ReadWrite"
-      create_option     = "FromImage"
-      managed_disk_type = "Standard_LRS"
-    }
-  
-    os_profile {
-      computer_name  = "hostname"
-      admin_username = "testadmin"
-      admin_password = "Password1234!"
-    }
-  
-    os_profile_linux_config {
-      disable_password_authentication = false
-    }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
   }
-  
-  resource "azurerm_network_interface" "main" {
-    name                = "${var.prefix}-nic"
-    location            = azurerm_resource_group.main.location
-    resource_group_name = azurerm_resource_group.main.name
-  
-    ip_configuration {
-      name                          = "internal"
-      subnet_id                     = azurerm_subnet.main.id
-      private_ip_address_allocation = "Dynamic"
-      public_ip_address_id = azurerm_public_ip.main.id
-    }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
-  
-  variable "prefix"{
-      default = "devops-ng"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
+}
+
+variable "prefix" {
+  default = "devops-ng"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "The username of the admin account"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "The password of the admin account"
+}
